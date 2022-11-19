7cc1005970e50a3c3dfbbaba6d4c91d30afa625c
ShitpostingAlt <manishman320@gmail.com>
2022-11-19 21:35:22 +0530

    Fixes

--- databse/db.js
+++ databse/db.js
@@ -1,10 +1,20 @@
+require("dotenv").config()
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/Gassword";
+const mongoURI = process.env.MONGO_URI
 
 const connectToMongo = () => {
-  mongoose.connect(mongoURI, () => {
-    console.log("Connected to MongoDB");
-  });
+  try {
+    mongoose.connect(mongoURI, () => {
+      console.log("Connected to MongoDB");
+    });
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error: "));
+    db.once("open", function () {
+      console.log("Connected successfully");
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 module.exports = connectToMongo;
--- server/core.js
+++ server/core.js
@@ -4,13 +4,15 @@ const connectToMongo = require("../databse/db");
 const app = express();
 const Auth = require("../routes/auth");
 const GasswordDB = require("../routes/gasswordDB")
+const cors = require("cors")
+app.use(cors())
 app.use(morgan("combined"));
 app.use(express.json())
 const port = 4000 || process.env.PORT;
 connectToMongo();
-// app.get("/", async (req, res) => {
-//     res.send("Hello")
-// })
+app.get("/", async (req, res) => {
+    res.send("Hello")
+})
 app.use("/auth/api", Auth);
 app.use("/api/gass" , GasswordDB)
 app.listen(port, () => {
